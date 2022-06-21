36f7825e4afaf0161d0e95731f61aa7e92ec44b0
Romain <contact@rominfo.fr>
2022-06-21 20:48:10 +0200

    first files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <a href="/">Home</a> |
+  <a href="/projects">Projects</a> |
+  <a href="/actions">Actions</a>
+  <component :is="currentView" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ActionCalendar from './components/ActionCalendar.vue'
+import ProjectList from './components/ProjectList.vue'
+import NotFound from './components/NotFound.vue'
+
+const routes = {
+  '/': HelloWorld,
+  '/projects': ProjectList,
+  '/actions': ActionCalendar
+}
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.history.pushState("", "", e.target.href);
+      this.currentPath = e.target.href
+    })
   }
 }
 </script>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -2,16 +2,14 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Projets
     </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'ActionCalendar',
   props: {
     msg: String
   }
